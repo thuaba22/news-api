e370e40ccacbeb9a473784296c58de5778fa1bd7
thuaba22 <mim.islam.tabassuma@gmail.com>
2023-08-30 22:27:34 +0600

    completed the catagory section on tab

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,15 @@
 const handleCatagory = async() => {
  const response = await fetch('https://openapi.programming-hero.com/api/news/categories');
  const data = await response.json();
- console.log(data);
+ const tabContainer = document.getElementById('tab-container');
+ data.data.news_category.slice(0,3).forEach((catagory) => {
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <a class="tab">${catagory.category_name}</a>
+    `;
+    tabContainer.appendChild(div);
+ });
  
 }
 
-handleCatagory()
+handleCatagory();
